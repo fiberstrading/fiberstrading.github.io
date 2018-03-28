abbda6e874452b34273384004e6086b4f48fd920
TRADER_PC6\User <ardywerkz19@gmail.com>
2018-03-28 18:54:48 +0800

    sdsddd

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
         loop:true,
         slideMove:2,
         easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
-        speed:600,
+        speed:900,
         pager: false,
         responsive : [
             {
@@ -36,4 +36,42 @@ $(document).ready(function(){
             }
         ]
 	  });
+	 $("#application").lightSlider({
+	    item:4,
+        auto:false,
+        loop:true,
+        slideMove:8,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed:1000,
+        pager: false,
+        responsive : [
+            {
+                breakpoint:991,
+                settings: {
+                    item:3,
+                    slideMove:1,
+                    slideMargin:6,
+                  }
+            },
+
+            {
+                breakpoint:767,
+                settings: {
+                    item:2,
+                    slideMove:1
+                  }
+            },
+
+            {
+                breakpoint:480,
+                settings: {
+                    item:1,
+                    slideMove:1
+                  }
+            }
+        ]
+	  });
+
+
+
 });
